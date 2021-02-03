0077a881217130b7a98f543ed1cffc2bcd43dddf
Arman D Haidarpour <arman0dh@gmail.com>
2021-02-03 22:09:47 +0330

    nav js

---
+++ dist/js/main.js
@@ -0,0 +1,34 @@
+// start nav-static script
+function sabadBtn (x) {
+    const sabadKaridBtn = document.getElementById("sabadKarid");
+    const loginBtnv = document.getElementById("loginBtn");
+    const btnLoginColor = document.getElementById("btnLoginColor");
+    const btnSabadColor = document.getElementById("btnSabadColor");
+    if (x == "sabadKarid") {
+        if (sabadKaridBtn.style.display === "none") {
+            sabadKaridBtn.style.display = "block";
+            btnSabadColor.style.background = "#f8edeb";
+            loginBtnv.style.display = "none";
+            btnLoginColor.style.background = "#fff";
+        }else {
+            sabadKaridBtn.style.display = "none";
+            btnSabadColor.style.background = "#fff";
+
+        }
+
+    }else if (x == "login") {
+        if (loginBtnv.style.display === "none") {
+            loginBtnv.style.display = "block";
+            sabadKaridBtn.style.display = "none";
+            btnSabadColor.style.background = "#fff";
+            btnLoginColor.style.background = "#f8edeb";
+        }else {
+            loginBtnv.style.display = "none";
+            btnLoginColor.style.background = "#fff";
+
+        }
+    }else {
+        console.log("?");
+    }
+}
+// end nav-static script
